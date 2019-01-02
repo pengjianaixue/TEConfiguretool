686b2777cf94cc92f83015681c29eb7503e72150
pengjianaixue <pengjianaixue@hotmail.com>
2019-01-02 10:57:44 +0800

    resource file

--- resource.h
+++ resource.h
@@ -4,4 +4,10 @@
 #include <sql.h>
 #include <sqlext.h>
 #include <QStyleFactory>
+#include <QStandardItem>
+#include <QStandardItemModel>
+#include <QTableView>
+#include <QAbstractItemModel>
+#include <QAbstractItemDelegate>
+#include <QAbstractItemView>
 #endif // RESOURCE_H
